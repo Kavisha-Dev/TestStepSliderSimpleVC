41387630452b7280030da6d896c3ff08c5e7b52f
kavisha <kavisha_jg@yahoo.com>
2020-01-29 18:22:41 +0530

    Initial Commit

---
+++ TestStepSliderSimpleVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestStepSliderSimpleVC
+//
+//  Created by Kavisha on 29/01/20.
+//  Copyright © 2020 SoKa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
